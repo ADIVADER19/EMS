77128394f9729a1ff294f62eb05eb40d3bb8a55a
Nurani <2317427@cognizant.com>
2024-03-11 16:06:06 +0530

    1st commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { TestComponent } from './test/test.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,TestComponent,NavbarComponent],
+  imports: [RouterOutlet,TestComponent,NavbarComponent,LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
